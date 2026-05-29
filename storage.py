0c4ce92042cdaa3ab0d381df39e96ea141748386
Gur-code <contact.gurjapan@gmail.com>
2026-05-29 11:11:36 -0400

    Change file paths in log and record functions
    Updated file paths in show_log and Personal_rec functions to absolute paths.

--- storage.py
+++ storage.py
@@ -1,4 +1,18 @@
 def show_log():
-    with open ("data.txt","r") as f:
+    with open (r"E:\Pythons folder\gym_tracker\data.txt","r") as f:
         abc = f.read()
         return abc
+
+
+def Personal_rec(word): 
+    with open(r"E:\Pythons folder\gym_tracker\data.txt","r") as f:
+        PR = 0
+        while True:
+            abc = f.readline()
+            if abc == "":
+                break
+            if (abc.find(word)!= -1):
+                W = f.readline()
+                if PR <= int(W[9:]):
+                    PR = int(W[9:])
+        print("Your personal record for",word,"is :",PR,"Kgs")
